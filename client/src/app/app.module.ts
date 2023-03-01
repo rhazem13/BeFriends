7015e867f851f82ee782a8fb00a5f215d57ab639
rhazem13 <rhazem13@yahoo.com>
2023-03-01 10:29:48 +0200

    create component chats

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -34,6 +34,7 @@ import { UserManagementComponent } from './admin/user-management/user-management
 import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
 import { RolesModalComponent } from './modals/roles-modal/roles-modal.component';
 import { ConfirmDialogComponent } from './modals/confirm-dialog/confirm-dialog.component';
+import { ChatsComponent } from './chats/chats.component';
 
 @NgModule({
   declarations: [
@@ -60,6 +61,7 @@ import { ConfirmDialogComponent } from './modals/confirm-dialog/confirm-dialog.c
     PhotoManagementComponent,
     RolesModalComponent,
     ConfirmDialogComponent,
+    ChatsComponent,
   ],
   imports: [
     BrowserModule,
