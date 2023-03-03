59813b596063d24c1249d58e8ee6d9a168f4dd31
rhazem13 <rhazem13@yahoo.com>
2023-03-04 01:11:21 +0200

    create sidenav

--- client/src/app/nav/sidenav-list/sidenav-list.component.ts
+++ client/src/app/nav/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AccountService } from 'src/app/services/account.service';
+import { BusyService } from 'src/app/services/busy.service';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -7,6 +11,33 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class SidenavListComponent {
   @Output() closeSidenav = new EventEmitter<void>();
+  model: any = {};
+
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    public busyService: BusyService
+  ) {}
+
+  login() {
+    this.accountService.login(this.model).subscribe(
+      (response) => {
+        this.router.navigateByUrl('/members');
+        console.log(response);
+      },
+      (error) => {
+        this.snackBar.open(error.error, undefined, {
+          duration: 1500,
+        });
+      }
+    );
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
+  }
 
   onClose() {
     this.closeSidenav.emit();
