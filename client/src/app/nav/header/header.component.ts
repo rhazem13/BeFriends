8ed1a287e5fcca4a2c2e735e6ea77ebb5b0d4da9
rhazem13 <rhazem13@yahoo.com>
2023-03-03 17:28:49 +0200

    edit nav

--- client/src/app/nav/header/header.component.ts
+++ client/src/app/nav/header/header.component.ts
@@ -1,4 +1,8 @@
+import { BusyService } from './../../services/busy.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +11,33 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent {
   @Output() sidenavToggle = new EventEmitter<void>();
+  model: any = {};
+
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    public busyService: BusyService
+  ) {}
+
+  login() {
+    this.accountService.login(this.model).subscribe(
+      (response) => {
+        this.router.navigateByUrl('/members');
+        console.log(response);
+      },
+      (error) => {
+        this.snackBar.open(error.error, undefined, {
+          duration: 1500,
+        });
+      }
+    );
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
+  }
 
   onToggleSidenav() {
     this.sidenavToggle.emit();
