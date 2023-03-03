6a9ee521f1c723dfb06bbc0df485e7fe62a60c2c
rhazem13 <rhazem13@yahoo.com>
2023-03-03 15:04:41 +0200

    edit nav

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -9,14 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   registerMode = false;
   constructor() {}
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
 
-  cancelRegisterMode(event: boolean){
+  cancelRegisterMode(event: boolean) {
     this.registerMode = event;
   }
 }
