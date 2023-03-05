3a549038165239198b3035592d13ca6aa0a158fc
rhazem13 <rhazem13@yahoo.com>
2023-03-05 18:54:43 +0200

    api fix bug where admin return in members list, ui fix pagination design

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -30,7 +30,7 @@ namespace API.Data
         {
             var query = context.Users
                .AsQueryable();
-            query = query.Where(u => u.UserName != userParams.CurrentUsername);
+            query = query.Where(u => u.UserName != userParams.CurrentUsername && u.UserName != "admin" );
             query = userParams.OrderBy switch
             {
                 "created" => query.OrderByDescending(u => u.Created),
