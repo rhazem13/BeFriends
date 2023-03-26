0e3a435f0530134b828f79787dabceb0100f0c29
rhazem13 <rhazem13@yahoo.com>
2023-03-26 21:51:13 +0200

    update home to redirect if loggedin

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { take } from 'rxjs';
+import { User } from '../models/user';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +11,24 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  user: User;
   registerMode = false;
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private accountService: AccountService,
+    private router: Router
+  ) {
+    this.accountService.currentUser$
+      .pipe(take(1))
+      .subscribe((user) => (this.user = user));
+  }
   ngOnInit(): void {
+    if(this.user){
+      this.router.navigate(['/feed'])
+    }
     this.route.data.subscribe((data) => {
       this.registerMode = data.registerMode;
     });
-
   }
 
   registerToggle() {
