4f3d3dc6976f00d21c44d74853e5d4783870cad8
rhazem13 <rhazem13@yahoo.com>
2023-07-07 04:04:32 +0300

    if user has no avatar post make it  default img

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'https://befriendsapi-ub24.onrender.com/api/',
-  hubUrl: 'https://befriendsapi-ub24.onrender.com/hubs/',
+  apiUrl: 'http://localhost:5221/api/',
+  hubUrl: 'http://localhost:5221/hubs/',
 };
